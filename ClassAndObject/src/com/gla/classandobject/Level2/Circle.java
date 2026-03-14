a11754bedb25bf7166e57439f945606ae8349e92
Ashita415 <ashitayadav009@gmail.com>
2026-03-14 20:09:30 +0530

    [Ashita] ClassAndObject Added

---
+++ ClassAndObject/src/com/gla/classandobject/Level2/Circle.java
@@ -0,0 +1,28 @@
+package com.gla.classandobject.Level2;
+import java.util.Scanner;
+
+class Circle {
+    double radius;
+
+    Circle() {
+        this(1);
+    }
+
+    Circle(double r) {
+        radius = r;
+    }
+
+    void display() {
+        System.out.println("Radius: " + radius);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Radius: ");
+        double r = sc.nextDouble();
+
+        Circle c = new Circle(r);
+        c.display();
+        sc.close();
+    }
+}
