418b0170b3a01d79f9c057aada7adc4e04fed3eb
Ashita415 <ashitayadav009@gmail.com>
2026-04-08 19:15:11 +0530

    [Ashita] Threads Added

---
+++ Threads/src/multithreading/bankaccountsimulation/Runner.java
@@ -0,0 +1,29 @@
+package multithreading.bankaccountsimulation;
+import java.util.*;
+    public class Runner {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter balance: ");
+            double balance=sc.nextDouble();
+
+            if(balance>100000) {
+                BankAccount user1 = new BankAccount("Sita", " Premium");
+                Thread t1=new Thread(user1);
+                t1.setPriority(10);
+                t1.start();
+            }
+            else if(balance>50000){
+                BankAccount user2=new BankAccount("Ram "," Regular");
+                Thread t2=new Thread(user2);
+                t2.setPriority(5);
+                t2.start();
+            }
+            else{
+                BankAccount user3=new BankAccount("Shayam "," Basic");
+                Thread t3=new Thread(user3);
+                t3.setPriority(1);
+                t3.start();
+            }
+
+        }
+    }
