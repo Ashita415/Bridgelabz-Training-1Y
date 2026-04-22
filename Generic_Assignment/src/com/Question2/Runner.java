517a0f66b9be0849e21a095cf7d42435f2dc1d1d
Ashita415 <ashitayadav009@gmail.com>
2026-04-22 22:43:08 +0530

    [Ashita] Added

---
+++ Generic_Assignment/src/com/Question2/Runner.java
@@ -0,0 +1,10 @@
+package com.Question2;
+
+public class Runner {
+        public static void main(String[] args) {
+            Pair<String, Integer> student = new Pair<>("Ashita Yadav", 22);
+
+            System.out.println("Name: " + student.getFirst());
+            System.out.println("Roll No.: " + student.getSecond());
+        }
+}
